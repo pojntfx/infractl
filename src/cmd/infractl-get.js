8e9212b182161c134c1961d6b3123db9960a4b6b
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-07-02 15:29:36 +0200

    feat: Add a network member get action

--- src/cmd/infractl-get.js
+++ src/cmd/infractl-get.js
@@ -8,4 +8,9 @@ commander
     "List all networks (without id) or get details on a network (with id)"
   )
   .alias("network")
+  .command(
+    "networkmembers",
+    "List all network members of a network (without id) or get details on a network member (with id)"
+  )
+  .alias("networkmember")
   .parse(process.argv);
