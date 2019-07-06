b050a329f8833a70e1ef3e496e5d13607b3d8cd4
Felix Pojtinger <felix@pojtinger.com>
2019-07-06 13:38:15 +0200

    feat: Add node delete action

--- src/cmd/infractl-delete.js
+++ src/cmd/infractl-delete.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
-require("../lib/asVerb")([["networks", "Delete network(s)", "network"]]);
+require("../lib/asVerb")([
+  ["networks", "Delete network(s)", "network"],
+  ["nodes", "Delete node(s)", "node"]
+]);
