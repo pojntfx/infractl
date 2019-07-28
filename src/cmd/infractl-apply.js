5080a818e1f7c18540da203bb8c9d1958cc80ae5
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-07-28 14:47:08 +0200

    feat(infractl): Add networkbinary apply action

--- src/cmd/infractl-apply.js
+++ src/cmd/infractl-apply.js
@@ -4,5 +4,6 @@ require("../lib/asVerb")([
   ["networks", "Create or update network", "network"],
   ["networkmembers", "Update network member", "networkmember"],
   ["nodes", "Create or update node", "node"],
-  ["sshkeys", "Create or update SSH key", "sshkey"]
+  ["sshkeys", "Create or update SSH key", "sshkey"],
+  ["networkbinaries", "Create or update network binary", "networkbinary"]
 ]);
