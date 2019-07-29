6c7813c5b216bd5bc4ba9c4df902e0a1b4281556
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-07-29 15:03:15 +0200

    feat(infractl): Add clusterworkers apply and delete actions

---
+++ src/cmd/infractl-apply-clusterworkers.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+const shell = require("shelljs");
+const fs = require("fs");
+const withSSH = require("../lib/withSSH");
+const withRsync = require("../lib/withRsync");
+const withPatches = require("../lib/withPatches");
+
+require("../lib/asGenericAction")({
+  args: "<user@ip> [otherTargets...]",
+  options: [
+    [
+      "-t, --cluster-token [clustertoken]",
+      "Worker's cluster token (i.e. bb25f9c6::node:6bf075c)"
+    ],
+    [
+      "-m, --manager [manager]",
+      "Worker's manager (i.e. https://195.201.22.140:6443)"
+    ]
+  ],
+  checker: commander => commander.clusterToken && commander.manager,
+  action: commander =>
+    fs.writeFile(
+      `${shell.tempdir()}/k3s-worker.service`,
+      `[Unit]
+Description=k3s kubernetes daemon (worker only)
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/k3s agent --no-flannel --token ${
+        commander.clusterToken
+      } --server ${commander.manager}
+
+[Install]
+WantedBy=multi-user.target
+`,
+      () =>
+        commander.args.map(target =>
+          withRsync({
+            source: `${shell.tempdir()}/k3s-worker.service`,
+            destination: `${target}:/etc/systemd/system/k3s-worker.service`,
+            permissions: "+rwx",
+            reUpload: commander.reUpload === "true"
+          }).then(() =>
+            withSSH(target, ssh =>
+              ssh
+                .execCommand(
+                  "systemctl daemon-reload && systemctl enable k3s-worker.service --now"
+                )
+                .then(() =>
+                  withPatches(ssh, () =>
+                    console.log(
+                      `Cluster worker successfully applied on ${target}.`
+                    )
+                  )
+                )
+            )
+          )
+        )
+    )
+});
