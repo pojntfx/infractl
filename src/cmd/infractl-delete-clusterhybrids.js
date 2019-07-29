e7ae05759e209806ef2c4a9f66cb851473ab9dbd
Felix Pojtinger <felix@pojtinger.com>
2019-07-29 14:42:31 +0200

    feat(infractl): Add cluster hybrid apply and delete actions

---
+++ src/cmd/infractl-delete-clusterhybrids.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const withSSH = require("../lib/withSSH");
+
+require("../lib/asGenericAction")({
+  args: "<user@ip> [otherTargets...]",
+  action: commander =>
+    commander.args.map(target =>
+      withSSH(target, ssh =>
+        ssh
+          .execCommand(
+            "systemctl disable k3s-hybrid.service --now && rm -f /etc/systemd/system/k3s-hybrid.service && systemctl daemon-reload"
+          )
+          .then(() => {
+            ssh.dispose();
+            console.log(`Cluster hybrid successfully deleted on ${target}.`);
+          })
+      )
+    )
+});
