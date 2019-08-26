6ded5bf9ccd894f027d9063415b5c5e5f06abea8
Felix Pojtinger <felix@pojtinger.com>
2019-08-26 14:58:43 +0200

    refactor(infractl): Delete kube-router interfaces in clusterconfig, not clusterdata

--- src/cmd/infractl-delete-clusterdatas.js
+++ src/cmd/infractl-delete-clusterdatas.js
@@ -11,16 +11,10 @@ require("../lib/asGenericAction")({
           .execCommand(
             "umount /var/lib/rancher/k3s/agent/kubelet/pods/*/volumes/kubernetes.io~secret/*; rm -rf /var/lib/rancher; rm -rf ${HOME}/.rancher; rm -rf /var/lib/cni; rm -rf /opt/cni; rm -rf /var/lib/kube-router"
           )
-          .then(() =>
-            ssh
-              .execCommand(
-                "ip link del kube-bridge; ip link del dummy0; ip link del kube-dummy-if"
-              )
-              .then(() => {
-                ssh.dispose();
-                console.log(`Cluster data successfully deleted on ${target}.`);
-              })
-          )
+          .then(() => {
+            ssh.dispose();
+            console.log(`Cluster data successfully deleted on ${target}.`);
+          })
       )
     )
 });
