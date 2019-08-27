8e5d5ee256942740ff1a731baa931f70b8ac1a81
Felix Pojtinger <felix@pojtinger.com>
2019-08-27 18:01:44 +0200

    feat(infractl): Remove all ZeroTier functionality as a response to their License change to BSL, prepare for switch to WireGuard

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,13 +3,6 @@ const package = require("../../package.json");
 
 const configstore = new Configstore(package.name);
 
-module.exports.setZeroTierConfig = options => {
-  options.zerotierApiEndpoint &&
-    configstore.set("zerotier.endpoint", options.zerotierApiEndpoint);
-  options.zerotierApiAccessToken &&
-    configstore.set("zerotier.accessToken", options.zerotierApiAccessToken);
-};
-
 module.exports.setHetznerCloudConfig = options => {
   options.hetznerCloudApiEndpoint &&
     configstore.set("hetznerCloud.endpoint", options.hetznerCloudApiEndpoint);
@@ -18,13 +11,6 @@ module.exports.setHetznerCloudConfig = options => {
       "hetznerCloud.accessToken",
       options.hetznerCloudApiAccessToken
     );
-};
-
-module.exports.withZeroTierConfig = async functionToGetConfig => {
-  functionToGetConfig({
-    endpoint: configstore.get("zerotier.endpoint"),
-    accessToken: configstore.get("zerotier.accessToken")
-  });
 };
 
 module.exports.withHetznerCloudConfig = async functionToGetConfig => {
--- src/lib/withZeroTierConfigCheck.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (config, functionToCall) => {
-  if (config.endpoint && config.accessToken) {
-    functionToCall(config);
-  } else {
-    console.error(
-      "The networks part (ZeroTier) of infractl has not yet been set up!"
-    );
-  }
-};
