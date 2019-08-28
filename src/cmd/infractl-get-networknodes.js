7375ebd7ee888188cfedbe556e6ac6777b93ed1d
Felix Pojtinger <felix@pojtinger.com>
2019-08-29 00:40:06 +0200

    feat(infractl): Add networknodes get action

---
+++ src/cmd/infractl-get-networknodes.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const withSSH = require("../lib/withSSH");
+const withTable = require("../lib/withTable");
+
+require("../lib/asGenericAction")({
+  args: "<user@ip>",
+  checker: commander => commander.args[0],
+  action: commander =>
+    withSSH(commander.args[0], ssh =>
+      ssh.execCommand(`cat /var/lib/wesher/state.json`).then(async result => {
+        if (result.stdout) {
+          const nodes = JSON.parse(result.stdout).Nodes;
+          const nodesWithOnlineStatus = [];
+          for (node of nodes) {
+            await ssh
+              .execCommand(`ping -c 1 ${node.OverlayAddr.IP}`)
+              .then(
+                res =>
+                  (res.stdout.includes("1 received") &&
+                    nodesWithOnlineStatus.push([
+                      node.PubKey,
+                      node.Name,
+                      true,
+                      node.OverlayAddr.IP
+                    ])) ||
+                  nodesWithOnlineStatus.push([
+                    node.PubKey,
+                    node.Name,
+                    false,
+                    node.OverlayAddr.IP
+                  ])
+              );
+          }
+          withTable({
+            headers: ["ID", "NAME", "ONLINE", "IP"],
+            data: nodesWithOnlineStatus
+          }).then(table => {
+            ssh.dispose();
+            console.log(table);
+          });
+        }
+      })
+    )
+});
