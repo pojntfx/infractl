31dac5851545c55d629a29e34ed6dcbc692b8799
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-08-29 01:38:30 +0200

    feat(infractl): Add networknodes get action for individual nodes, fix loop in networkworkers apply action

--- src/cmd/infractl-get-networknodes.js
+++ src/cmd/infractl-get-networknodes.js
@@ -4,41 +4,55 @@ const withSSH = require("../lib/withSSH");
 const withTable = require("../lib/withTable");
 
 require("../lib/asGenericAction")({
-  args: "<user@ip>",
-  checker: commander => commander.args[0],
+  args: "[id]",
+  options: [
+    ["-n, --node <user@ip>", "Node from which to get the network members"]
+  ],
+  checker: commander => commander.node,
   action: commander =>
-    withSSH(commander.args[0], ssh =>
+    withSSH(commander.node, ssh =>
       ssh.execCommand(`cat /var/lib/wesher/state.json`).then(async result => {
         if (result.stdout) {
           const nodes = JSON.parse(result.stdout).Nodes;
-          const nodesWithOnlineStatus = [];
-          for (node of nodes) {
-            await ssh
-              .execCommand(`ping -c 1 ${node.OverlayAddr.IP}`)
-              .then(
-                res =>
-                  (res.stdout.includes("1 received") &&
+          if (commander.args[0]) {
+            console.log(
+              JSON.stringify(
+                nodes.find(node => node.PubKey === commander.args[0]),
+                null,
+                4
+              )
+            );
+            ssh.dispose();
+          } else {
+            const nodesWithOnlineStatus = [];
+            for (node of nodes) {
+              await ssh
+                .execCommand(`ping -c 1 ${node.OverlayAddr.IP}`)
+                .then(
+                  res =>
+                    (res.stdout.includes("1 received") &&
+                      nodesWithOnlineStatus.push([
+                        node.PubKey,
+                        node.Name,
+                        true,
+                        node.OverlayAddr.IP
+                      ])) ||
                     nodesWithOnlineStatus.push([
                       node.PubKey,
                       node.Name,
-                      true,
+                      false,
                       node.OverlayAddr.IP
-                    ])) ||
-                  nodesWithOnlineStatus.push([
-                    node.PubKey,
-                    node.Name,
-                    false,
-                    node.OverlayAddr.IP
-                  ])
-              );
+                    ])
+                );
+            }
+            withTable({
+              headers: ["ID", "NAME", "READY", "IP"],
+              data: nodesWithOnlineStatus
+            }).then(table => {
+              ssh.dispose();
+              console.log(table);
+            });
           }
-          withTable({
-            headers: ["ID", "NAME", "READY", "IP"],
-            data: nodesWithOnlineStatus
-          }).then(table => {
-            ssh.dispose();
-            console.log(table);
-          });
         }
       })
     )
