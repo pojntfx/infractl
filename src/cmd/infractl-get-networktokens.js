470f3e37088b6c9486da4036ac4d5a5e244a5a09
Felix Pojtinger <felix@pojtinger.com>
2019-08-29 12:34:25 +0200

    feat(infractl): Use alternative networktoken get action to fix state.json not created problem

--- src/cmd/infractl-get-networktokens.js
+++ src/cmd/infractl-get-networktokens.js
@@ -7,9 +7,17 @@ require("../lib/asGenericAction")({
   checker: commander => commander.args[0],
   action: commander =>
     withSSH(commander.args[0], ssh =>
-      ssh.execCommand(`cat /var/lib/wesher/state.json`).then(result => {
-        ssh.dispose();
-        console.log(result.stdout && JSON.parse(result.stdout).ClusterKey);
-      })
+      ssh
+        .execCommand(`cat /etc/systemd/system/wesher-manager.service`)
+        .then(result => {
+          ssh.dispose();
+          console.log(
+            result.stdout &&
+              result.stdout
+                .split("--cluster-key")[1]
+                .split("\n")[0]
+                .replace(" ", "")
+          );
+        })
     )
 });
