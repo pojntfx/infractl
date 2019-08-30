107a676c385db841942d12c9030df7e6208ecb3b
Felix Pojtinger <felix@pojtinger.com>
2019-08-30 13:59:44 +0200

    refactor(infractl): Decompose networknodes get action

--- src/cmd/infractl-get-networknodes.js
+++ src/cmd/infractl-get-networknodes.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const withSSH = require("../lib/withSSH");
 const withTable = require("../lib/withTable");
+const getNetworknodes = require("../lib/actions/getNetworknodes");
 
 require("../lib/asGenericAction")({
   args: "[id]",
@@ -10,64 +10,13 @@ require("../lib/asGenericAction")({
   ],
   checker: commander => commander.node,
   action: commander =>
-    withSSH(commander.node, ssh =>
-      ssh.execCommand(`cat /var/lib/wesher/state.json`).then(async result => {
-        if (result.stdout) {
-          const nodes = JSON.parse(result.stdout).Nodes;
-          if (commander.args[0]) {
-            console.log(
-              JSON.stringify(
-                nodes.find(node => node.PubKey === commander.args[0]),
-                null,
-                4
-              )
-            );
-            ssh.dispose();
-          } else {
-            const nodesWithOnlineStatus = [];
-            for (node of nodes) {
-              await ssh
-                .execCommand(`ping -c 1 ${node.OverlayAddr.IP}`)
-                .then(
-                  res =>
-                    (res.stdout.includes("1 received") &&
-                      nodesWithOnlineStatus.push([
-                        node.PubKey,
-                        node.Name,
-                        true,
-                        node.OverlayAddr.IP
-                      ])) ||
-                    nodesWithOnlineStatus.push([
-                      node.PubKey,
-                      node.Name,
-                      false,
-                      node.OverlayAddr.IP
-                    ])
-                );
-            }
-            await ssh.execCommand(`ip a`).then(res =>
-              ssh.execCommand(`hostname`).then(res2 =>
-                nodesWithOnlineStatus.unshift([
-                  "NOT AVAILABLE (YOU ARE QUERYING VIA THIS NODE)",
-                  res2.stdout,
-                  true,
-                  res.stdout
-                    .split("wgoverlay")[1]
-                    .split("inet")[1]
-                    .split("/32")[0]
-                    .replace(" ", "")
-                ])
-              )
-            );
-            withTable({
+    getNetworknodes({ target: commander.args[0], node: commander.node }).then(
+      nodes =>
+        nodes.list
+          ? withTable({
               headers: ["ID", "NAME", "READY", "IP"],
-              data: nodesWithOnlineStatus
-            }).then(table => {
-              ssh.dispose();
-              console.log(table);
-            });
-          }
-        }
-      })
+              data: nodes.data
+            }).then(table => console.log(table))
+          : console.log(nodes.data)
     )
 });
