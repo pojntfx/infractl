f170a89a05a936ee0a7f7550844d060aba5ca937
Felix Pojtinger <felix@pojtinger.com>
2019-08-30 17:27:50 +0200

    refactor(infractl): Decompose clusterworker actions

--- src/cmd/infractl-apply-clusterworkers.js
+++ src/cmd/infractl-apply-clusterworkers.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
-const shell = require("shelljs");
-const fs = require("fs");
-const withSSH = require("../lib/withSSH");
-const withRsync = require("../lib/withRsync");
-const withPatches = require("../lib/withPatches");
+const {
+  writeClusterworker,
+  uploadClusterworker
+} = require("../lib/actions/applyClusterworker");
 
 require("../lib/asGenericAction")({
   args: "<user@ip> [otherTargets...]",
@@ -13,46 +12,26 @@ require("../lib/asGenericAction")({
       "-t, --cluster-token [clustertoken]",
       "Worker's cluster token (i.e. bb25f9c6::node:6bf075c)"
     ],
-    ["-m, --manager [manager]", "Worker's manager (i.e. 195.201.22.140)"]
+    ["-m, --manager [manager]", "Worker's manager (i.e. 195.201.22.140)"],
+    [
+      "-u, --re-upload [true|false]",
+      "Whether the networkworker should be uploaded again if it already exists on the target (default false)"
+    ]
   ],
   checker: commander => commander.clusterToken && commander.manager,
   action: commander =>
-    fs.writeFile(
-      `${shell.tempdir()}/k3s-worker.service`,
-      `[Unit]
-Description=k3s kubernetes daemon (worker only)
-After=network.target
-
-[Service]
-ExecStart=/usr/local/bin/k3s agent --no-flannel --token ${commander.clusterToken} --server https://${commander.manager}:6443
-
-[Install]
-WantedBy=multi-user.target
-`,
-      () =>
-        commander.args.map(target =>
-          withRsync({
-            source: `${shell.tempdir()}/k3s-worker.service`,
-            destination: `${target}:/etc/systemd/system/k3s-worker.service`,
-            permissions: "+rwx",
-            reUpload: commander.reUpload === "true"
-          }).then(() =>
-            withSSH(target, ssh =>
-              ssh
-                .execCommand(
-                  `systemctl daemon-reload;
-systemctl enable k3s-worker.service --now;`
-                )
-                .then(() =>
-                  withPatches(ssh, ssh => {
-                    ssh.dispose();
-                    console.log(
-                      `Cluster worker successfully applied on ${target}.`
-                    );
-                  })
-                )
-            )
-          )
+    writeClusterworker({
+      clusterToken: commander.clusterToken,
+      manager: commander.manager
+    }).then(source =>
+      commander.args.map(target =>
+        uploadClusterworker({
+          source,
+          target,
+          reUpload: commander.reUpload
+        }).then(target =>
+          console.log(`Cluster worker successfully applied on ${target}.`)
         )
+      )
     )
 });
