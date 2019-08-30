ae574da2bb79afa3709beb4c71dc2dde6fd0f6f7
Felix Pojtinger <felix@pojtinger.com>
2019-08-30 13:50:13 +0200

    refactor(infractl): Decompose networktoken get action

--- src/cmd/infractl-get-networktokens.js
+++ src/cmd/infractl-get-networktokens.js
@@ -1,23 +1,12 @@
 #!/usr/bin/env node
 
-const withSSH = require("../lib/withSSH");
+const getNetworktoken = require("../lib/actions/getNetworktoken");
 
 require("../lib/asGenericAction")({
   args: "<user@ip>",
   checker: commander => commander.args[0],
   action: commander =>
-    withSSH(commander.args[0], ssh =>
-      ssh
-        .execCommand(`cat /etc/systemd/system/wesher-manager.service`)
-        .then(result => {
-          ssh.dispose();
-          console.log(
-            result.stdout &&
-              result.stdout
-                .split("--cluster-key")[1]
-                .split("\n")[0]
-                .replace(" ", "")
-          );
-        })
+    getNetworktoken(commander.args[0]).then(
+      token => token && console.log(token)
     )
 });
