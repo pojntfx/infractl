9a756d0072d952458113643b75e133d00aee8ac7
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-08-31 18:29:28 +0200

    refactor(infractl): Make all nodes actions classes

--- src/cmd/infractl-get-nodes.js
+++ src/cmd/infractl-get-nodes.js
@@ -1,8 +1,40 @@
 #!/usr/bin/env node
 
-const getNode = require("../lib/actions/getNode");
+const Nodes = require("../lib/models/nodes");
+const withTable = require("../lib/withTable");
+const YAML = require("yaml");
 
 require("../lib/asHetznerCloudAction")({
   args: "[id]",
-  action: (commander, cloud) => getNode({ id: commander.args[0], cloud })
+  action: (commander, cloud) => {
+    const nodes = new Nodes(cloud);
+    nodes.get(commander.args[0]).then(nodes =>
+      nodes.list
+        ? withTable({
+            headers: ["ID", "NAME", "READY", "IP", "OS", "TYPE", "LOCATION"],
+            data: nodes.data.servers.map(
+              ({
+                id,
+                name,
+                status,
+                public_net: {
+                  ipv4: { ip }
+                },
+                server_type: { name: serverType },
+                datacenter: { name: location },
+                image: { name: os }
+              }) => [
+                id,
+                name,
+                status === "running",
+                ip,
+                os,
+                serverType,
+                location
+              ]
+            )
+          }).then(table => console.log(table))
+        : console.log(YAML.stringify(nodes.data, null, 4))
+    );
+  }
 });
