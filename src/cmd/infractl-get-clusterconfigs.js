ca62bb2c1d6150ca4af74e68adc2a1a6133b8787
Felix Pojtinger <felix@pojtinger.com>
2019-08-31 11:57:22 +0200

    refactor(infractl): Decompose clusterconfig actions

--- src/cmd/infractl-get-clusterconfigs.js
+++ src/cmd/infractl-get-clusterconfigs.js
@@ -1,17 +1,12 @@
 #!/usr/bin/env node
 
-const withSSH = require("../lib/withSSH");
+const getClusterconfig = require("../lib/actions/getClusterconfig");
 
 require("../lib/asGenericAction")({
   args: "<user@ip>",
   checker: commander => commander.args[0],
   action: commander =>
-    withSSH(commander.args[0], ssh =>
-      ssh.execCommand(`cat /etc/rancher/k3s/k3s.yaml`).then(result => {
-        ssh.dispose();
-        console.log(
-          result.stdout.replace("localhost", commander.args[0].split("@")[1])
-        );
-      })
+    getClusterconfig(commander.args[0]).then(
+      config => config && console.log(config)
     )
 });
