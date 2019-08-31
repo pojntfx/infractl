74cad6c6fca95546dc925040399fcd1c7fb461c8
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-08-31 18:18:20 +0200

    refactor(infractl): Add nodes apply method

--- src/cmd/infractl-apply-nodes.js
+++ src/cmd/infractl-apply-nodes.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const applyNode = require("../lib/actions/applyNode");
+const Nodes = require("../lib/models/nodes");
+const withTable = require("../lib/withTable");
 
 require("../lib/asHetznerCloudAction")({
   args: "[id]",
@@ -31,15 +33,25 @@ require("../lib/asHetznerCloudAction")({
     commander.location ||
     commander.poweredOn ||
     commander.sshKeys,
-  action: (commander, cloud) =>
-    applyNode({
-      id: commander.args[0],
-      name: commander.nodeName,
-      image: commander.operatingSystem,
-      nodeType: commander.nodeType,
-      location: commander.location,
-      sshKeys: commander.sshKeys,
-      poweredOn: commander.poweredOn,
-      cloud
-    })
+  action: (commander, cloud) => {
+    const nodes = new Nodes(cloud);
+    nodes
+      .apply({
+        id: commander.args[0],
+        name: commander.nodeName,
+        operatingSystem: commander.operatingSystem,
+        nodeType: commander.nodeType,
+        location: commander.location,
+        sshKeys: commander.sshKeys,
+        poweredOn: commander.poweredOn,
+        cloud
+      })
+      .then(({ id, name, status, ip, os, serverType, location }) => {
+        withTable({
+          preceedingText: "Node successfully applied:",
+          headers: ["ID", "NAME", "READY", "IP", "OS", "TYPE", "LOCATION"],
+          data: [[id, name, status === "running", ip, os, serverType, location]]
+        }).then(table => console.log(table));
+      });
+  }
 });
---
+++ src/lib/models/nodes.js
@@ -0,0 +1,96 @@
+module.exports = class {
+  constructor(cloud) {
+    this.cloud = cloud;
+  }
+
+  async apply({
+    id,
+    name,
+    operatingSystem,
+    nodeType,
+    location,
+    sshKeys,
+    poweredOn
+  }) {
+    return new Promise(resolve =>
+      this.cloud
+        .upsertNode(id || undefined, {
+          name: name || undefined,
+          image: (!id && operatingSystem) || undefined,
+          server_type: (!id && nodeType) || undefined,
+          location: (!id && location) || undefined,
+          ssh_keys: (!id && sshKeys.split(",")) || undefined,
+          start_after_create: !id
+            ? poweredOn === "false"
+              ? false
+              : poweredOn === true
+              ? true
+              : undefined
+            : undefined
+        })
+        .then(
+          ({
+            server: {
+              id,
+              name,
+              status,
+              public_net: {
+                ipv4: { ip }
+              },
+              server_type: { name: serverType },
+              datacenter: { name: location },
+              image: { name: os }
+            }
+          }) => {
+            id
+              ? poweredOn === "false"
+                ? this.cloud.updateNodeStatus(id, false).then(updatedNode =>
+                    resolve({
+                      id,
+                      name,
+                      status: updatedNode.status === "running",
+                      ip,
+                      serverType,
+                      location,
+                      os
+                    })
+                  )
+                : poweredOn === "true"
+                ? this.cloud.updateNodeStatus(id, true).then(updatedNode =>
+                    resolve({
+                      id,
+                      name,
+                      status: updatedNode.status === "running",
+                      ip,
+                      serverType,
+                      location,
+                      os
+                    })
+                  )
+                : resolve({
+                    id,
+                    name,
+                    status: status === "running",
+                    ip,
+                    serverType,
+                    location,
+                    os
+                  })
+              : resolve({
+                  id,
+                  name,
+                  status: status === "running",
+                  ip,
+                  serverType,
+                  location,
+                  os
+                });
+          }
+        )
+    );
+  }
+
+  async get() {}
+
+  async delete() {}
+};
