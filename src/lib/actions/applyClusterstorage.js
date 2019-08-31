02ae67868b124d5720d8606f1f8115cecef3155b
Felix Pojtinger <felix@pojtinger.com>
2019-08-31 14:20:31 +0200

    refactor(infractl): Decompose clusterstorage actions

---
+++ src/lib/actions/applyClusterstorage.js
@@ -0,0 +1,153 @@
+const { Client, KubeConfig } = require("kubernetes-client");
+const Request = require("kubernetes-client/backends/request");
+const longhornNamespace = require("../../data/longhorn-namespace.json");
+const longhornClusterrole = require("../../data/longhorn-clusterrole.json");
+const longhornClusterrolebinding = require("../../data/longhorn-clusterrolebinding.json");
+const longhornCustomresourcedefinitions = require("../../data/longhorn-customresourcedefinitions.json");
+const longhornDaemonset = require("../../data/longhorn-daemonset.json");
+const longhornService = require("../../data/longhorn-service.json");
+const longhornServiceaccount = require("../../data/longhorn-serviceaccount.json");
+const longhornDeployment = require("../../data/longhorn-deployment.json");
+const longhornStorageclass = require("../../data/longhorn-storageclass.json");
+const _ = require("lodash");
+
+module.exports = async clusterconfig =>
+  new Promise(async resolve => {
+    let client = {};
+    if (clusterconfig) {
+      const kubeconfig = new KubeConfig();
+      kubeconfig.loadFromFile(clusterconfig);
+      const backend = new Request({ kubeconfig });
+      client = new Client({
+        backend
+      });
+    } else {
+      client = new Client();
+    }
+    await client.loadSpec();
+    await client.api.v1.namespaces
+      .post({ body: longhornNamespace })
+      .catch(
+        async () =>
+          await client.api.v1
+            .namespaces("longhorn-system")
+            .put({ body: longhornNamespace })
+      );
+    await client.apis["rbac.authorization.k8s.io"].v1beta1.clusterroles
+      .post({
+        body: longhornClusterrole
+      })
+      .catch(
+        async () =>
+          await client.apis["rbac.authorization.k8s.io"].v1beta1
+            .clusterroles("longhorn-role")
+            .put({
+              body: longhornClusterrole
+            })
+      );
+    await client.apis["rbac.authorization.k8s.io"].v1beta1.clusterrolebindings
+      .post({
+        body: longhornClusterrolebinding
+      })
+      .catch(
+        async () =>
+          await client.apis["rbac.authorization.k8s.io"].v1beta1
+            .clusterrolebindings("longhorn-bind")
+            .put({
+              body: longhornClusterrolebinding
+            })
+      );
+    for (let definition of longhornCustomresourcedefinitions) {
+      await client.apis[
+        "apiextensions.k8s.io"
+      ].v1beta1.customresourcedefinitions
+        .post({
+          body: definition
+        })
+        .catch(
+          async () =>
+            await client.apis["apiextensions.k8s.io"].v1beta1
+              .customresourcedefinitions(definition.metadata.name)
+              .put({
+                body: _.merge(definition, {
+                  metadata: {
+                    resourceVersion: (await client.apis[
+                      "apiextensions.k8s.io"
+                    ].v1beta1
+                      .customresourcedefinitions(definition.metadata.name)
+                      .get()).body.metadata.resourceVersion
+                  }
+                })
+              })
+        );
+      await client.addCustomResourceDefinition(definition);
+    }
+    await client.apis.apps.v1beta2
+      .namespaces("longhorn-system")
+      .daemonsets.post({ body: longhornDaemonset })
+      .catch(
+        async () =>
+          await client.apis.apps.v1beta2
+            .namespaces("longhorn-system")
+            .daemonsets("longhorn-manager")
+            .put({ body: longhornDaemonset })
+      );
+    await client.api.v1
+      .namespaces("longhorn-system")
+      .services.post({ body: longhornService })
+      .catch(
+        async () =>
+          await client.api.v1
+            .namespaces("longhorn-system")
+            .services("longhorn-backend")
+            .put({
+              body: _.merge(longhornService, {
+                metadata: {
+                  resourceVersion: (await client.api.v1
+                    .namespaces("longhorn-system")
+                    .services("longhorn-backend")
+                    .get()).body.metadata.resourceVersion
+                },
+                spec: {
+                  clusterIP: (await client.api.v1
+                    .namespaces("longhorn-system")
+                    .services("longhorn-backend")
+                    .get()).body.spec.clusterIP
+                }
+              })
+            })
+      );
+    await client.api.v1
+      .namespaces("longhorn-system")
+      .serviceaccounts.post({ body: longhornServiceaccount })
+      .catch(
+        async () =>
+          await client.api.v1
+            .namespaces("longhorn-system")
+            .serviceaccounts("longhorn-service-account")
+            .put({ body: longhornServiceaccount })
+      );
+    await client.apis.apps.v1beta2
+      .namespaces("longhorn-system")
+      .deployments.post({ body: longhornDeployment })
+      .catch(
+        async () =>
+          await client.apis.apps.v1beta2
+            .namespaces("longhorn-system")
+            .deployments("longhorn-driver-deployer")
+            .put({ body: longhornDeployment })
+      );
+    await client.apis["storage.k8s.io"].v1.storageclasses
+      .post({
+        body: longhornStorageclass
+      })
+      .catch(
+        async () =>
+          await client.apis["storage.k8s.io"].v1
+            .storageclasses("longhorn")
+            .put({
+              body: longhornStorageclass
+            })
+      );
+    await resolve(true);
+  });
---
+++ src/lib/actions/deleteClusterstorage.js
@@ -0,0 +1,102 @@
+const { Client, KubeConfig } = require("kubernetes-client");
+const Request = require("kubernetes-client/backends/request");
+const longhornDeleteServiceaccount = require("../../data/longhorn-delete-serviceaccount.json");
+const longhornDeleteClusterrole = require("../../data/longhorn-delete-clusterrole.json");
+const longhornDeleteClusterrolebinding = require("../../data/longhorn-delete-clusterrolebinding.json");
+const longhornDeleteJob = require("../../data/longhorn-delete-job.json");
+const withFinishedJob = require("../withFinishedJob");
+const longhornCustomresourcedefinitions = require("../../data/longhorn-customresourcedefinitions.json");
+
+module.exports = async clusterconfig =>
+  new Promise(async resolve => {
+    let client = {};
+    if (clusterconfig) {
+      const kubeconfig = new KubeConfig();
+      kubeconfig.loadFromFile(clusterconfig);
+      const backend = new Request({ kubeconfig });
+      client = new Client({
+        backend
+      });
+    } else {
+      client = new Client();
+    }
+    await client.loadSpec();
+    await client.apis["storage.k8s.io"].v1.storageclasses("longhorn").delete();
+    await client.api.v1
+      .namespaces("default")
+      .serviceaccounts.post({ body: longhornDeleteServiceaccount })
+      .catch(
+        async () =>
+          await client.api.v1
+            .namespaces("default")
+            .serviceaccounts("longhorn-uninstall-service-account")
+            .put({ body: longhornDeleteServiceaccount })
+      );
+    await client.apis["rbac.authorization.k8s.io"].v1beta1.clusterroles
+      .post({
+        body: longhornDeleteClusterrole
+      })
+      .catch(
+        async () =>
+          await client.apis["rbac.authorization.k8s.io"].v1beta1
+            .clusterroles("longhorn-uninstall-role")
+            .put({
+              body: longhornDeleteClusterrole
+            })
+      );
+    await client.apis["rbac.authorization.k8s.io"].v1beta1.clusterrolebindings
+      .post({
+        body: longhornDeleteClusterrolebinding
+      })
+      .catch(
+        async () =>
+          await client.apis["rbac.authorization.k8s.io"].v1beta1
+            .clusterrolebindings("longhorn-uninstall-bind")
+            .put({
+              body: longhornDeleteClusterrolebinding
+            })
+      );
+    await client.apis.batch.v1
+      .namespaces("default")
+      .job.post({
+        body: longhornDeleteJob
+      })
+      .catch(
+        async () =>
+          await client.apis.batch.v1
+            .namespaces("default")
+            .job("longhorn-uninstall")
+            .put({
+              body: longhornDeleteJob
+            })
+      );
+    await withFinishedJob(client, async () => {
+      await client.apis.batch.v1
+        .namespaces("default")
+        .job("longhorn-uninstall")
+        .delete();
+      for (let definition of longhornCustomresourcedefinitions) {
+        await client.apis["apiextensions.k8s.io"].v1beta1
+          .customresourcedefinitions(definition.metadata.name)
+          .delete();
+      }
+      await client.apis["rbac.authorization.k8s.io"].v1beta1
+        .clusterrolebindings("longhorn-bind")
+        .delete();
+      await client.apis["rbac.authorization.k8s.io"].v1beta1
+        .clusterroles("longhorn-role")
+        .delete();
+      await client.api.v1.namespaces("longhorn-system").delete();
+      await client.api.v1
+        .namespaces("default")
+        .serviceaccounts("longhorn-uninstall-service-account")
+        .delete();
+      await client.apis["rbac.authorization.k8s.io"].v1beta1
+        .clusterrolebindings("longhorn-uninstall-bind")
+        .delete();
+      await client.apis["rbac.authorization.k8s.io"].v1beta1
+        .clusterroles("longhorn-uninstall-role")
+        .delete();
+      await resolve(true);
+    });
+  });
