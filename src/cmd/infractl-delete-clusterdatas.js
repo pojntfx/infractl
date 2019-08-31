a18bdbb2f4bccb458a36e029b0137947ac1d29ba
Felix Pojtinger <felix@pojtinger.com>
2019-08-31 12:17:56 +0200

    refactor(infractl): Decompose clusterdata delete action

--- src/cmd/infractl-delete-clusterdatas.js
+++ src/cmd/infractl-delete-clusterdatas.js
@@ -1,20 +1,13 @@
 #!/usr/bin/env node
 
-const withSSH = require("../lib/withSSH");
+const deleteClusterdata = require("../lib/actions/deleteClusterdata");
 
 require("../lib/asGenericAction")({
   args: "<user@ip> [otherTargets...]",
   action: commander =>
     commander.args.map(target =>
-      withSSH(target, ssh =>
-        ssh
-          .execCommand(
-            "umount /var/lib/rancher/k3s/agent/kubelet/pods/*/volumes/kubernetes.io~secret/*; rm -rf /var/lib/rancher; rm -rf ${HOME}/.rancher; rm -rf /var/lib/cni; rm -rf /opt/cni; rm -rf /var/lib/kube-router"
-          )
-          .then(() => {
-            ssh.dispose();
-            console.log(`Cluster data successfully deleted on ${target}.`);
-          })
+      deleteClusterdata(target).then(target =>
+        console.log(`Cluster data successfully deleted on ${target}.`)
       )
     )
 });
