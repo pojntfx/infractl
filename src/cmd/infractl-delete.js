1345982dc037be1bfafd3b3152f2fb114a4a70dd
Felix Pojtinger <felix@pojtinger.com>
2019-08-31 23:06:13 +0200

    docs(infractl): Use more sane delete documentation layout

--- src/cmd/infractl-delete.js
+++ src/cmd/infractl-delete.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 
 require("../lib/asVerb")([
-  ["networkworkers", "Delete network worker(s)", "networkworker"],
-  ["networkmanagers", "Delete network manager(s)", "networkmanager"],
-  ["networkbinaries", "Delete network binary/binaries", "networkbinary"],
-  ["networkdatas", "Delete network data(s)", "networkdata"],
   ["clusterstorages", "Delete cluster storage", "clusterstorage"],
   ["clusterrouters", "Delete cluster router", "clusterrouter"],
   ["clusterworkers", "Delete cluster worker(s)", "clusterworker"],
@@ -13,6 +9,10 @@ require("../lib/asVerb")([
   ["clusterbinaries", "Delete cluster binary/binaries", "clusterbinary"],
   ["clusterconfigs", "Delete cluster config(s)", "clusterconfig"],
   ["clusterdatas", "Delete cluster data(s)", "clusterdata"],
+  ["networkworkers", "Delete network worker(s)", "networkworker"],
+  ["networkmanagers", "Delete network manager(s)", "networkmanager"],
+  ["networkbinaries", "Delete network binary/binaries", "networkbinary"],
+  ["networkdatas", "Delete network data(s)", "networkdata"],
   ["nodes", "Delete node(s)", "node"],
   ["sshkeys", "Delete SSH key(s)", "sshkey"]
 ]);
