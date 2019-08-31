3e6e566ed3aa9a24b38a214473bd4ab6b39c5fca
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-08-31 14:04:17 +0200

    refactor(infractl): Decompose clusterrouter delete action

--- src/cmd/infractl-delete-clusterrouters.js
+++ src/cmd/infractl-delete-clusterrouters.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-const { Client, KubeConfig } = require("kubernetes-client");
-const Request = require("kubernetes-client/backends/request");
+const deleteClusterrouter = require("../lib/actions/deleteClusterrouter");
 
 require("../lib/asGenericAction")({
   options: [
@@ -10,51 +9,8 @@ require("../lib/asGenericAction")({
       "Path to the kubeconfig to work with (default ~/.kube/config)"
     ]
   ],
-  action: async commander => {
-    let client = {};
-    if (commander.clusterconfig) {
-      const kubeconfig = new KubeConfig();
-      kubeconfig.loadFromFile(commander.clusterconfig);
-      const backend = new Request({ kubeconfig });
-      client = new Client({
-        backend
-      });
-    } else {
-      client = new Client();
-    }
-    await client.loadSpec();
-    await client.api.v1
-      .namespaces("kube-system")
-      .configmaps("kube-proxy")
-      .delete();
-    await client.api.v1
-      .namespaces("kube-system")
-      .configmaps("kube-router-cfg")
-      .delete();
-    await client.apis.extensions.v1beta1
-      .namespaces("kube-system")
-      .daemonsets("kube-router")
-      .delete();
-    for (pod of (await client.api.v1
-      .namespaces("kube-system")
-      .pods.get()).body.items
-      .filter(pod => pod.metadata.name.includes("kube-router-"))
-      .map(pod => pod.metadata.name)) {
-      await client.api.v1
-        .namespaces("kube-system")
-        .pods(pod)
-        .delete();
-    }
-    await client.api.v1
-      .namespaces("kube-system")
-      .serviceaccounts("kube-router")
-      .delete();
-    await client.apis["rbac.authorization.k8s.io"].v1beta1
-      .clusterroles("kube-router")
-      .delete();
-    await client.apis["rbac.authorization.k8s.io"].v1beta1
-      .clusterrolebindings("kube-router")
-      .delete();
-    console.log(`Cluster router successfully deleted.`);
-  }
+  action: async commander =>
+    deleteClusterrouter(commander.clusterconfig).then(() =>
+      console.log(`Cluster router successfully deleted.`)
+    )
 });
