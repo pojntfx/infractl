53278b31388bb25c8066bcfdbfed9064b010fe78
Felix Pojtinger <felix@pojtinger.com>
2019-08-31 12:15:32 +0200

    refactor(infractl): Decompose the clusternode get action

--- src/cmd/infractl-get-clusternodes.js
+++ src/cmd/infractl-get-clusternodes.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-const { Client, KubeConfig } = require("kubernetes-client");
-const Request = require("kubernetes-client/backends/request");
 const withTable = require("../lib/withTable");
+const getClusternode = require("../lib/actions/getClusternode");
 
 require("../lib/asGenericAction")({
   args: "[name]",
@@ -12,40 +11,25 @@ require("../lib/asGenericAction")({
       "Path to the kubeconfig to work with (default ~/.kube/config)"
     ]
   ],
-  action: async commander => {
-    let client = {};
-    if (commander.clusterconfig) {
-      const kubeconfig = new KubeConfig();
-      kubeconfig.loadFromFile(commander.clusterconfig);
-      const backend = new Request({ kubeconfig });
-      client = new Client({
-        backend
-      });
-    } else {
-      client = new Client();
-    }
-    await client.loadSpec();
-    if (commander.args[0]) {
-      await client.api.v1
-        .nodes(commander.args[0])
-        .get()
-        .then(res => console.log(JSON.stringify(res.body, null, 4)));
-    } else {
-      await client.api.v1.nodes.get().then(res =>
-        withTable({
-          headers: ["NAME", "READY"],
-          data: res.body.items.map(node => [
-            node.metadata.name,
-            JSON.stringify(
-              !(
-                node.status.conditions.find(
-                  condition => condition.type === "Ready"
-                ).status === "False"
+  action: async commander =>
+    getClusternode({
+      clusterconfig: commander.clusterconfig,
+      name: commander.args[0]
+    }).then(nodes =>
+      nodes.list
+        ? withTable({
+            headers: ["NAME", "READY"],
+            data: nodes.data.map(node => [
+              node.metadata.name,
+              JSON.stringify(
+                !(
+                  node.status.conditions.find(
+                    condition => condition.type === "Ready"
+                  ).status === "False"
+                )
               )
-            )
-          ])
-        }).then(table => console.log(table))
-      );
-    }
-  }
+            ])
+          }).then(table => console.log(table))
+        : console.log(JSON.stringify(nodes.data, null, 4))
+    )
 });
