d79be078d85f4e5587ebce108b67518b4a778a97
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-09-01 10:35:43 +0200

    feat(infractl): Enable deployment on localhost

--- src/lib/withSSH.js
+++ src/lib/withSSH.js
@@ -1,12 +1,32 @@
 const SSH = require("node-ssh");
+const shell = require("shelljs");
 
 module.exports = async (address, functionToCall) => {
   const ssh = new SSH();
-  ssh
-    .connect({
-      host: address.split("@")[1],
-      username: address.split("@")[0],
-      agent: process.env.SSH_AUTH_SOCK
-    })
-    .then(() => functionToCall(ssh));
+  const host = address.split("@")[1];
+  const username = address.split("@")[0];
+  if (host === "localhost" && username === process.env.USER) {
+    const SSHLocal = class {
+      constructor() {
+        this.isLocal = true;
+      }
+
+      async execCommand(command) {
+        return new Promise(resolve => {
+          resolve(shell.exec(command));
+        });
+      }
+
+      dispose() {}
+    };
+    functionToCall(new SSHLocal());
+  } else {
+    ssh
+      .connect({
+        host,
+        username,
+        agent: process.env.SSH_AUTH_SOCK
+      })
+      .then(() => functionToCall(ssh));
+  }
 };
