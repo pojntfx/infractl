b1f9e14e5846ff093e1abde24c9a8774c304569c
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-09-04 14:42:14 +0200

    feat(infractl): Add network manager and worker service creators to lean action

--- src/cmd/infractl-apply-leans.js
+++ src/cmd/infractl-apply-leans.js
@@ -15,49 +15,68 @@ require("../lib/asGenericAction")({
     const allNodes = [networkManagerNode, ...networkWorkerNodes];
     // Download network binaries
     const downloader = new Downloader();
-    console.log("[INFO] Downloading wireguard-go");
-    const wireguardGoSource = await downloader.download(
+    console.log("[INFO] Downloading network core binary");
+    const networkDriverBinarySource = await downloader.download(
       "https://nx904.your-next.cloud/s/9JSS9BsQEQTEW8E/download",
       "/tmp/wireguard-go"
     );
-    console.log("[INFO] Downloading wesher");
-    const wesherSource = await downloader.download(
+    console.log("[INFO] Downloading network interface binary");
+    const networkInterfaceBinarySource = await downloader.download(
       "https://nx904.your-next.cloud/s/NLk8NdCPf4GqkZ9/download",
       "/tmp/wesher"
     );
     // Upload network binaries
     const uploader = new Uploader();
-    console.log("[INFO] Uploading wireguard-go");
+    console.log("[INFO] Uploading network core binary");
     await Promise.all(
       allNodes.map(node =>
         uploader.upload(
-          wireguardGoSource,
+          networkDriverBinarySource,
           `${node}:/usr/local/bin/wireguard-go`
         )
       )
     );
-    console.log("[INFO] Uploading wesher");
+    console.log("[INFO] Uploading network interface binary");
     await Promise.all(
       allNodes.map(node =>
-        uploader.upload(wesherSource, `${node}:/usr/local/bin/wesher`)
+        uploader.upload(
+          networkInterfaceBinarySource,
+          `${node}:/usr/local/bin/wesher`
+        )
       )
     );
     // Set network binaries' permissions
     const permissioner = new Permissioner();
-    console.log("[INFO] Set permissions for wireguard-go");
+    console.log("[INFO] Setting permissions for network core binary");
     await Promise.all(
       allNodes.map(node =>
         permissioner.setPermissions(`${node}:/usr/local/bin/wireguard-go`, "+x")
       )
     );
-    console.log("[INFO] Set permissions for wesher");
+    console.log("[INFO] Setting permissions for network interface binary");
     await Promise.all(
       allNodes.map(node =>
         permissioner.setPermissions(`${node}:/usr/local/bin/wesher`, "+x")
       )
     );
     // Create manager and worker services
-    const servicer = new Servicer();
     const cryptographer = new Cryptographer();
+    console.log("[INFO] Creating network key");
+    const networkKey = await cryptographer.getRandomString(32);
+    console.log("[INFO] Creating network manager service");
+    const servicer = new Servicer();
+    const networkManagerServiceSource = await servicer.createService({
+      description: "Overlay network daemon (manager and worker)",
+      execStart: `/usr/local/bin/wesher --cluster-key ${networkKey}`,
+      destination: "/tmp/network-manager.service"
+    });
+    console.log("[INFO] Creating network worker service");
+    const networkWorkerServiceSource = await servicer.createService({
+      description: "Overlay network daemon (manager and worker)",
+      execStart: `/usr/local/bin/wesher --cluster-key ${networkKey} --join ${
+        networkManagerNode.split("@")[1]
+      }`,
+      destination: "/tmp/network-worker.service"
+    });
   }
 });
