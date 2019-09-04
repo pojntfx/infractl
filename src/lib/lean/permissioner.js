0f4916efe1c327c9fb97d25676655eac08c0c995
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-09-04 12:02:14 +0200

    feat(infractl): Add permissioner to lean apply action

--- src/lib/lean/permissioner.js
+++ src/lib/lean/permissioner.js
@@ -1,14 +1,14 @@
 const SSH = require("node-ssh");
 
 module.exports = class {
-  async upload(source, destination) {
+  async setPermissions(destination, permissions) {
     const ssh = new SSH();
     await ssh.connect({
       host: destination.split("@")[1].split(":")[0],
       username: destination.split("@")[0],
       agent: process.env.SSH_AUTH_SOCK
     });
-    await ssh.putFile(source, destination.split(":")[1]);
+    await ssh.execCommand(`chmod ${permissions} ${destination.split(":")[1]}`);
     ssh.dispose();
     return true;
   }
