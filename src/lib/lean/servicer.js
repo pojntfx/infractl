a3ea01d48a8b7a2331a9d3fe6ed0837e0f04c4bb
Felix Pojtinger <felix@pojtinger.com>
2019-09-04 14:42:14 +0200

    feat(infractl): Add network manager and worker service creators to lean action

--- src/lib/lean/servicer.js
+++ src/lib/lean/servicer.js
@@ -1,5 +1,22 @@
 const fs = require("fs");
 
 module.exports = class {
-  async createService({ name, description, execStart }) {}
+  async createService({ description, execStart, destination }) {
+    return new Promise(resolve =>
+      fs.writeFile(
+        destination,
+        `[Unit]
+Description=${description}
+After=network.target
+
+[Service]
+ExecStart=${execStart}
+
+[Install]
+WantedBy=multi-user.target
+`,
+        () => resolve(destination)
+      )
+    );
+  }
 };
