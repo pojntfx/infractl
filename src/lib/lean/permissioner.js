d4181008baebcb84b382dc750aec2fc397b0bd4b
Felix Pojtinger <felix@pojtinger.com>
2019-09-05 00:31:28 +0200

    feat(infractl): Install IPTables in infractl apply action

--- src/lib/lean/permissioner.js
+++ src/lib/lean/permissioner.js
@@ -3,9 +3,13 @@ const SSHer = require("./ssher");
 module.exports = class {
   async setPermissions(destination, permissions) {
     const ssher = new SSHer(destination.split(":")[0]);
-    await ssher.execCommand(
-      `chmod ${permissions} ${destination.split(":")[1]}`
-    );
+    if (ssher.isLocal) {
+      await ssher.shell.chmod(destination.split(":")[1], permissions);
+    } else {
+      await ssher.execCommand(
+        `chmod ${permissions} ${destination.split(":")[1]}`
+      );
+    }
     return true;
   }
 };
--- src/lib/lean/uploader.js
+++ src/lib/lean/uploader.js
@@ -4,6 +4,6 @@ module.exports = class {
   async upload(source, destination) {
     const ssher = new SSHer(destination.split(":")[0]);
     await ssher.putFile(source, destination.split(":")[1]);
-    return true;
+    return destination;
   }
 };
