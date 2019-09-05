4fa56b707685f59d749fc373b089e94283eec68f
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-09-05 17:33:02 +0200

    feat(infractl): Get cluster token in lean apply action

--- src/lib/lean/cater.js
+++ src/lib/lean/cater.js
@@ -4,18 +4,22 @@ const fs = require("fs");
 module.exports = class {
   async getFileContent(destination, asJSON) {
     const ssher = new SSHer(destination.split(":")[0]);
-    let fileContent = "";
+    let fileContent = false;
     if (ssher.isLocal) {
-      fileContent = await new Promise(resolve =>
-        fs.readFile(
-          destination.split("@")[1].split(":")[1],
-          "UTF8",
-          (_, data) => resolve(data)
-        )
-      );
+      fileContent = await new Promise(resolve => {
+        fs.existsSync(destination.split("@")[1].split(":")[1])
+          ? fs.readFile(
+              destination.split("@")[1].split(":")[1],
+              "UTF8",
+              (_, data) => resolve(data)
+            )
+          : resolve(false);
+      });
     } else {
       const fileContentRaw = await ssher.execCommand(
-        `cat ${destination.split("@")[1].split(":")[1]}`
+        `[ -f ${destination.split("@")[1].split(":")[1]} ] && cat ${
+          destination.split("@")[1].split(":")[1]
+        }`
       );
       fileContent = fileContentRaw;
     }
