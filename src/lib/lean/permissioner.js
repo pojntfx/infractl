2b3b38adc8d3e661ab0b669cb82d68af33d201f5
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-09-05 03:16:36 +0200

    refactor(infractl): Make local commands more silent

--- src/lib/lean/permissioner.js
+++ src/lib/lean/permissioner.js
@@ -3,13 +3,7 @@ const SSHer = require("./ssher");
 module.exports = class {
   async setPermissions(destination, permissions) {
     const ssher = new SSHer(destination.split(":")[0]);
-    if (ssher.isLocal) {
-      await ssher.shell.chmod(permissions, destination.split(":")[1]);
-    } else {
-      await ssher.execCommand(
-        `chmod ${permissions} ${destination.split(":")[1]}`
-      );
-    }
+    await ssher.chmod(permissions, destination.split(":")[1]);
     return true;
   }
 };
