9e267e0372e01c04c2de4d3e9f814f89a99c4015
Felix Pojtinger <felix@pojtinger.com>
2019-09-05 03:45:14 +0200

    fix(infractl): Keep permissioner in seperate module

--- src/lib/lean/permissioner.js
+++ src/lib/lean/permissioner.js
@@ -3,7 +3,13 @@ const SSHer = require("./ssher");
 module.exports = class {
   async setPermissions(destination, permissions) {
     const ssher = new SSHer(destination.split(":")[0]);
-    await ssher.chmod(permissions, destination.split(":")[1]);
+    if (ssher.isLocal) {
+      await ssher.shell.chmod(permissions, destination.split(":")[1]);
+    } else {
+      await ssher.execCommand(
+        `chmod ${permissions} ${destination.split(":")[1]}`
+      );
+    }
     return true;
   }
 };
