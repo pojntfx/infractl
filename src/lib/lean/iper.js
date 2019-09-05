e20e5c4414b3ad283d86a2c54793a1034c14b4b6
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-09-05 02:53:44 +0200

    refactor(infractl): Use `os` instead of `shelljs` for local IP address gathering

--- src/lib/lean/iper.js
+++ src/lib/lean/iper.js
@@ -1,31 +1,45 @@
 const SSHer = require("./ssher");
+const os = require("os");
 
 module.exports = class {
   async getAllInterfaces(destination) {
     const ssher = new SSHer(destination);
-    const rawInterfaces = await ssher.execCommand("ip a");
-    const interfaces = rawInterfaces
-      .split("\n")
-      .filter(line => line.includes("<"))
-      .map(line =>
-        line
-          .split(":")[1]
-          .split(":")[0]
-          .replace(" ", "")
-      );
-    return interfaces;
+    if (ssher.isLocal) {
+      return Object.keys(os.networkInterfaces());
+    } else {
+      const rawInterfaces = await ssher.execCommand("ip a");
+      const interfaces = rawInterfaces
+        .split("\n")
+        .filter(line => line.includes("<"))
+        .map(line =>
+          line
+            .split(":")[1]
+            .split(":")[0]
+            .replace(" ", "")
+        );
+      return interfaces;
+    }
   }
 
   async getInterface(destination, name) {
     const ssher = new SSHer(destination);
-    const rawInterface = await ssher.execCommand(`ip addr show ${name}`);
-    return {
-      name,
-      ip: rawInterface
-        .split("inet")[1]
-        .split("/")[0]
-        .replace(" ", "")
-    };
+    if (ssher.isLocal) {
+      return {
+        name,
+        ip: os
+          .networkInterfaces()
+          [name].find(subInterface => subInterface.family === "IPv4")["address"]
+      };
+    } else {
+      const rawInterface = await ssher.execCommand(`ip addr show ${name}`);
+      return {
+        name,
+        ip: rawInterface
+          .split("inet")[1]
+          .split("/")[0]
+          .replace(" ", "")
+      };
+    }
   }
 
   async waitForInterface(destination, name, interval) {
