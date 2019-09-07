a3a859261181126eff53120a6480ca46c336d343
Felix Pojtinger <felix@pojtinger.com>
2019-09-07 14:48:34 +0200

    refactor(infractl): Use new k3s version

--- src/lib/lean/clusterer.js
+++ src/lib/lean/clusterer.js
@@ -30,7 +30,7 @@ module.exports = class {
     const rawClusterConfig = await cater.getFileContent(
       `${destination}:/etc/rancher/k3s/k3s.yaml`
     );
-    return rawClusterConfig.replace(/localhost/g, ip);
+    return rawClusterConfig.replace(/127\.0\.0\.1/g, ip);
   }
 
   async waitForClusterConfig(destination, interval) {
