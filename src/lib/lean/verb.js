32d7289d219a78919778349f15caf9a1b282fce1
Felix Pojtinger <felix@pojtinger.com>
2019-09-10 20:44:10 +0000

    feat(infractl): Add app class

--- src/lib/lean/verb.js
+++ src/lib/lean/verb.js
@@ -2,9 +2,10 @@ const commander = require("commander");
 
 module.exports = class {
   constructor(commands) {
-    commands.forEach(command => {
-      commander.command(command[0], command[1]).alias(command[2]);
-    });
+    Array.isArray(commands) &&
+      commands.forEach(command => {
+        commander.command(command[0], command[1]).alias(command[2]);
+      });
 
     commander.parse(process.argv);
   }
