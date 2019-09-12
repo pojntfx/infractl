10aa00101d6759def64ef36fb1d38ab831bedbf6
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-09-12 09:43:31 +0000

    feat(infractl): Add nodes get action

--- src/lib/universaler.js
+++ src/lib/universaler.js
@@ -47,4 +47,57 @@ module.exports = class {
       return false;
     }
   }
+
+  async getSupracloudNodeId(from, id) {
+    if (from === "hetzner") {
+      return `H-${id}`;
+    } else {
+      return false;
+    }
+  }
+
+  async getProprietaryNodeId(to, id) {
+    if (to === "hetzner") {
+      return id.replace("H-", "");
+    } else {
+      return false;
+    }
+  }
+
+  async getSupracloudNode(from, node, isUniversalId) {
+    if (from === "hetzner") {
+      return {
+        id: isUniversalId
+          ? node.server
+            ? node.server.id
+            : node.id
+          : await this.getSupracloudNodeId(
+              "hetzner",
+              node.server ? node.server.id : node.id
+            ),
+        name: node.server ? node.server.name : node.name,
+        publicAccess: node.server
+          ? `root@${node.server.public_net.ipv4.ip}`
+          : `root@${node.public_net.ipv4.ip}`,
+        os: node.server ? node.server.image.name : node.image.name,
+        cores: node.server
+          ? node.server.server_type.cores
+          : node.server_type.cores,
+        memory: node.server
+          ? node.server.server_type.memory
+          : node.server_type.memory,
+        disk: node.server ? node.server.server_type.disk : node.server_type.disk
+      };
+    } else {
+      return false;
+    }
+  }
+
+  async getSupracloudNodeList(from, keys) {
+    if (from === "hetzner") {
+      return keys.servers;
+    } else {
+      return false;
+    }
+  }
 };
