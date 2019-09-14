1905fa17db02c136b8b72ea80ac868f5be1ff15b
Felix Pojtinger <felix@pojtinger.com>
2019-09-14 20:09:20 +0000

    docs(infractl): Document the placement of target arguments

--- src/cmd/infractl-delete.js
+++ src/cmd/infractl-delete.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
 new (require("../lib/verb"))([
-  ["clusters", "Delete cluster(s)", "cluster"],
+  ["workloadclusters", "Delete workload cluster(s)", "workloadcluster"],
+  [
+    "privatenetworkclusters",
+    "Delete private network cluster(s)",
+    "privatenetworkcluster"
+  ],
   ["nodes", "Delete node(s)", "node"],
   ["keys", "Delete key(s)", "key"]
 ]);
