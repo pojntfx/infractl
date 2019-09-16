dc5ae8e74f442134e1b42c261851190bc8e2901d
Felix Pojtinger <felix@pojtinger.com>
2019-09-16 12:48:02 +0000

    feat(infractl): Add key apply action

---
+++ src/cmd/infractl-apply-keys.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+const Logger = require("../lib/logger");
+const Hostnamer = require("../lib/hostnamer");
+const Contexter = require("../lib/contexter");
+const packageJSON = require("../../package.json");
+const Universaler = require("../lib/universaler");
+const Hetzner = require("../lib/hetzner");
+const Cater = require("../lib/cater");
+
+new (require("../lib/noun"))({
+  args: `<id|"H-">`,
+  options: [
+    ["-n, --key-name [name]", "Key's name"],
+    ["-f, --key-file [path]", "Key's path (cannot be updated)"]
+  ],
+  checker: commander =>
+    commander.args[0]
+      ? commander.args[0].split(/[A-Z]\-/)[1]
+        ? commander.keyName && !commander.keyFile
+        : commander.keyName && commander.keyFile
+      : false,
+  action: async commander => {
+    const hostnamer = new Hostnamer();
+    const localhost = hostnamer.getAddress();
+    const logger = new Logger();
+    const contexter = new Contexter(packageJSON.name);
+    const universaler = new Universaler();
+    const cater = new Cater();
+
+    // Create clients
+    const hetzner = new Hetzner({
+      endpoint: await contexter.getHetznerEndpoint(),
+      token: await contexter.getHetznerToken()
+    });
+
+    let sshKey = undefined;
+
+    // Hetzner
+    if (commander.args[0].split("-")[0] === "H") {
+      sshKey = await hetzner.upsertSSHKey(
+        commander.args[0]
+          ? await universaler.getProprietarySSHKeyId(
+              "hetzner",
+              commander.args[0]
+            )
+          : false,
+        {
+          name: commander.keyName,
+          public_key: await cater.getFileContent(
+            `${localhost}:${commander.keyFile}`
+          )
+        }
+      );
+      return sshKey.error
+        ? await logger.log(localhost, sshKey.error.message, "error")
+        : await logger.log(
+            localhost,
+            await universaler.getSupracloudSSHKey(
+              "hetzner",
+              sshKey,
+              true,
+              false
+            ),
+            "data",
+            "Successfully applied key"
+          );
+    } else {
+      return await logger.log(
+        localhost,
+        `Not a valid id, key provider prefix "${
+          commander.args[0].split("-")[0]
+        }" is not supported!`,
+        "error"
+      );
+    }
+  }
+});
