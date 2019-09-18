49c5316f1815acc058140c26ae2b21485965af61
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-09-18 13:26:27 +0000

    refactor(infractl): Use universal binaries for N2N instead of RPM/DEB packages

--- src/cmd/infractl-apply-privatenetworkclusters.js
+++ src/cmd/infractl-apply-privatenetworkclusters.js
@@ -392,7 +392,7 @@ new (require("../lib/noun"))({
     const managerServiceSource = isType2
       ? await servicer.createService({
           description: "Network cluster daemon (manager and worker)",
-          execStart: `/bin/sh -c "/usr/sbin/supernode -l 9090 -v & /usr/sbin/edge -d edge0 -r -a 192.168.1.1 -c privatenetworkcluster -k ${token} -l localhost:9090 -v && /usr/sbin/dhcpd -f edge0"`,
+          execStart: `/bin/sh -c "/usr/local/bin/supernode -l 9090 -v & /usr/local/bin/edge -d edge0 -r -a 192.168.1.1 -c privatenetworkcluster -k ${token} -l localhost:9090 -v && /usr/sbin/dhcpd -f edge0"`,
           destination: await tmpFiler.getPath(
             "private-network-cluster-manager.service"
           )
@@ -413,7 +413,7 @@ new (require("../lib/noun"))({
     const workerServiceSources = isType2
       ? await servicer.createService({
           description: "Network cluster daemon (worker only)",
-          execStart: `/bin/sh -c "/usr/sbin/edge -d edge0 -r -a dhcp:0.0.0.0 -c privatenetworkcluster -k ${token} -l ${
+          execStart: `/bin/sh -c "/usr/local/bin/edge -d edge0 -r -a dhcp:0.0.0.0 -c privatenetworkcluster -k ${token} -l ${
             publicManagerNode[0].split("@")[1]
           }:9090 -v -m $(echo $(cat /etc/machine-id)|md5sum|sed 's/^\\(..\\)\\(..\\)\\(..\\)\\(..\\)\\(..\\).*$/02:\\1:\\2:\\3:\\4:\\5/') && pkill -9 dhclient; /sbin/dhclient edge0; tail -f /dev/null"`,
           destination: await tmpFiler.getPath(
