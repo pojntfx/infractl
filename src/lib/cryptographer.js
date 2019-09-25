2bdfae6084de2bc0c8b75a32da8361c83ed3d651
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-09-25 00:53:31 +0000

    feat(infractl): Add S3-compatible automatic PVC backup system

--- src/lib/cryptographer.js
+++ src/lib/cryptographer.js
@@ -4,4 +4,8 @@ module.exports = class {
   async getRandomString(length) {
     return crypto.randomBytes(length).toString("base64");
   }
+
+  async getBase64(input) {
+    return Buffer.from(input).toString("base64");
+  }
 };
